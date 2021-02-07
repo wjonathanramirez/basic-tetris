7fb327701ead98c3666533eca8745956da1609b3
wjonathanramirez <wjonathanramirez@gmail.com>
2021-02-06 17:40:50 -0700

    Define grids

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array(document.querySelectorAll('.grid div'))
+    const width = 10;
+
+    console.log(grid);
+    console.log(squares);
 
 
 
